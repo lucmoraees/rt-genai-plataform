f459ce1dfe554ceb405317e18fdb5382d4466629
lucmoraees <lucmoraes27@hotmail.com>
2025-06-29 12:36:15 -0300

    [update] update README

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 origins = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+  "http://localhost:8000",
+  "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
