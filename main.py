d0371ca6dd8bf4462c988dbeb19d977ba08ad949
lucmoraees <lucmoraes27@hotmail.com>
2025-06-29 02:44:51 -0300

    [update] add cors configuration

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from app.controllers.history_controller import router as history_router
 from infra.clients.mongo_client import MongoClient
 from infra.exceptions.handlers import register_exception_handlers
 from infra.logging.logging_config import configure_logging
-
-configure_logging()
+from fastapi.middleware.cors import CORSMiddleware
 
 async def lifespan(app: FastAPI):
   MongoClient.connect()
@@ -13,6 +12,23 @@ async def lifespan(app: FastAPI):
   yield
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+configure_logging()
 register_exception_handlers(app)
+
+# Routes
 app.include_router(chat_router)
 app.include_router(history_router)
